1b307e5f4529f825d8cb3b0ffd89e823b1463d93
daniel anggrianto <d.anggrianto@gmail.com>
2022-04-15 10:00:37 -0400

    change domain

--- custom_components/apex_fusion/__init__.py
+++ custom_components/apex_fusion/__init__.py
@@ -1,8 +1,8 @@
-DOMAIN = "apex"
+DOMAIN = "apex_fusion"
 
 
 def setup(hass, config):
-    hass.states.set("apex.hello", "true")
+    hass.states.set("apex_fusion.hello", "true")
 
     # Return boolean to indicate that initialization was successful.
     return True
