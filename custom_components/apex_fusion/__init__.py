2c1feb7750ff0cb3b6b66914286d2c1d78c279a6
daniel anggrianto <d.anggrianto@gmail.com>
2022-04-15 10:15:25 -0400

    sample hello world

--- custom_components/apex_fusion/__init__.py
+++ custom_components/apex_fusion/__init__.py
@@ -1,8 +1,16 @@
+from __future__ import annotations
+
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.typing import ConfigType
+
+# The domain of your component. Should be equal to the name of your component.
 DOMAIN = "apex_fusion"
 
 
-def setup(hass, config):
-    hass.states.set("apex_fusion.hello", "true")
+def setup(hass: HomeAssistant, config: ConfigType) -> bool:
+    """Set up a skeleton component."""
+    # States are in the format DOMAIN.OBJECT_ID.
+    hass.states.set('apex_fusion.Hello_World', 'Works!')
 
-    # Return boolean to indicate that initialization was successful.
+    # Return boolean to indicate that initialization was successfully.
     return True
